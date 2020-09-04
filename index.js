6c34f196405dcd6be810e5e3c4a15ec8e2c11017
Basil Homadi <deviltearbasil@gmail.com>
2020-09-04 14:35:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 function greet(time) {
-  const hour = 
+  const hour = parseInt()
   if (time < 12) {
     return "Good Morning"
   }
